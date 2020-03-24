1603fa4d82debd66af3791ab8a3f0969d114e1ef
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-24 14:53:21 +0000

    Deploy ufologist/page-schema-player to github.com/ufologist/page-schema-player.git:gh-pages

--- 61.9bdb57d.js
+++ 61.9bdb57d.js
@@ -1,4 +1,4 @@
-/*! page-schema-player | 4f4b92e 2020-03-24 22:30:28 +0800 | (c) Sun */
+/*! page-schema-player | 06c8be2 2020-03-24 22:49:30 +0800 | (c) Sun */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[61],{1841:function(e,o,t){"use strict";t.r(o),t.d(o,"conf",(function(){return n})),t.d(o,"language",(function(){return i}));
 /*!---------------------------------------------------------------------------------------------
  *  Copyright (C) David Owens II, owensd.io. All rights reserved.
